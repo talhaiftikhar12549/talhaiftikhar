c41696a0c3bfc0d489f0472edcc1504fa0248a91
talhaiftikhar12549 <talhaiftikhar12549@gmail.com>
2024-12-14 15:48:43 +0500

    added particle js library

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-
+import ParticlesComponent from './components/Particle';
 function App() {
 
 
   return (
     <>
+    <ParticlesComponent id="particles" />
      <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
