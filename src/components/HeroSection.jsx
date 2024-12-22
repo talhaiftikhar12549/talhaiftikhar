f9e141da6beea53d38865fe28c0eee6fd93e8ad4
talhaiftikhar12549 <talhaiftikhar12549@gmail.com>
2024-12-22 14:51:55 +0500

    hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,11 +4,11 @@ export default function HeroSection() {
             <section className=' text-[#ffffff] border-rose-600 border-2  absolute' >
                 <div className="flex">
                    <div>
-                    <p>Hello,i'm</p>
-                    <p>Talha</p>
-                    <p>a passsionate Software Engineer</p>
+                    <p className="text-2xl font-bold">Hello 👋,i'm</p>
+                    <p className="text-5xl font-bold">Talha</p>
+                    <p className="text-2xl font-bold">a passsionate Software Engineer</p>
                     </div>
-                   <div></div>
+                   <div><img src="" alt="" /></div>
                 </div>
             </section>
         </>
