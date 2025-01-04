08a1702342217c8e09af454b7b6bef18ba44beb7
Talha <talhaiftikhar12549@gmail.com>
2025-01-04 16:50:38 +0500

    Added Responsiveness to Testimonail

--- src/components/TestamonialSection.jsx
+++ src/components/TestamonialSection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import UforiaLogo from "../assets/uforiainfotech_logo-removebg-preview.png";
+import usmanPic from "../assets/usmanPic.jpg";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import { Pagination, Autoplay } from "swiper/modules";
@@ -10,31 +11,29 @@ import "swiper/css/autoplay";
 const Carousel = () => {
   return (
     <>
-      <div className="h-[60vh] w-full bg-[#0e0e1a] py-8 flex items-center justify-center">
+      <div className="h-[100vh] sm:h-[80vh] lg:h-[60vh]  w-full bg-[#0e0e1a] flex items-center justify-center">
         <Swiper
-          modules={[Pagination, Autoplay]} 
-          autoplay={{ delay: 3000 }} 
-          loop={true} 
-          slidesPerView={1} 
-          className="w-[50%] h-[80%]"
-          pagination={{ clickable: true }} 
+          modules={[Pagination, Autoplay]}
+          autoplay={{ delay: 3000 }}
+          loop={true}
+          slidesPerView={1}
+          className="lg:w-[85%] xl:w-[90%] 2xl:w-[50%] h-[80%]"
+          pagination={{ clickable: true }}
           style={{
             '--swiper-pagination-color': '#2c2ebf',
             '--swiper-pagination-bullet-inactive-color': '#cccccc',
           }}
         >
           <SwiperSlide>
-            <div className="border-x-[12px] border-[#0e0e1a] bg-[#060614] px-4 w-[100%] h-[70%] flex items-center justify-evenly">
-              <div className="flex items-center justify-center w-[20%]">
-                <img className="h-[120px] w-[120px]" src={UforiaLogo} alt="Uforia Logo" />
+            <div className="border-x-[12px] border-[#0e0e1a] bg-[#060614] px-4 w-[100%] h-[70%]  flex flex-col lg:flex-row items-center justify-evenly">
+              <div className="flex items-center justify-center w-[100%] lg:w-[20%]">
+                <img className="h-[120px] w-[120px]" src={usmanPic} alt="usmanPic" />
               </div>
               <div className="flex flex-col w-[80%] text-[#ffffff]">
                 <p>
                   <span className="text-[#2c2ebf] text-3xl">″</span>
-                  <span className="text-[#ffffff] text-sm font-light">
-                    Using the Scrum Board has completely transformed the way our team collaborates.
-                    The intuitive design and seamless functionality make tracking tasks effortless.
-                    It's an essential tool for staying organized and delivering projects on time!
+                  <span className="text-[#ffffff] text-xs md:text-sm font-light text-wrap">
+                  I have worked with this buddy for a long time and have witnessed his growth in his career. We have collaborated on different projects, and he helped me rank a website on the first page.
                   </span>
                   <span className="text-[#2c2ebf] text-3xl">″</span>
                 </p>
@@ -45,22 +44,59 @@ const Carousel = () => {
           </SwiperSlide>
 
           <SwiperSlide>
-            <div className="border-x-[12px] border-[#0e0e1a] bg-[#060614] px-4 w-[100%] h-[70%] flex items-center justify-evenly">
-              <div className="flex items-center justify-center w-[20%]">
+            <div className="border-x-[12px] border-[#0e0e1a] bg-[#060614] px-4 w-[100%] h-[70%]  flex flex-col lg:flex-row items-center justify-evenly">
+              <div className="flex items-center justify-center w-[100%] lg:w-[20%]">
                 <img className="h-[120px] w-[120px]" src={UforiaLogo} alt="Uforia Logo" />
               </div>
               <div className="flex flex-col w-[80%] text-[#ffffff]">
                 <p>
                   <span className="text-[#2c2ebf] text-3xl">″</span>
-                  <span className="text-[#ffffff] text-sm font-light">
+                  <span className="text-[#ffffff] text-xs md:text-sm font-light text-wrap">
                     Using the Scrum Board has completely transformed the way our team collaborates.
                     The intuitive design and seamless functionality make tracking tasks effortless.
-                    It's an essential tool for staying organized and delivering projects on time!
                   </span>
                   <span className="text-[#2c2ebf] text-3xl">″</span>
                 </p>
-                <p className="font-semibold py-3">Usman Tariq</p>
-                <p className="text-xs font-light">UI/UX designer at Veroke</p>
+                <p className="font-semibold py-3">Usama Malik</p>
+                <p className="text-xs font-light">Software Engineer at Uforia</p>
+              </div>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <div className="border-x-[12px] border-[#0e0e1a] bg-[#060614] px-4 w-[100%] h-[70%] flex flex-col lg:flex-row items-center justify-evenly">
+              <div className="flex items-center justify-center w-[100%] lg:w-[20%]">
+                <img className="h-[120px] w-[120px]" src={UforiaLogo} alt="Uforia Logo" />
+              </div>
+              <div className="flex flex-col w-[80%] text-[#ffffff]">
+                <p>
+                  <span className="text-[#2c2ebf] text-3xl">″</span>
+                  <span className="text-[#ffffff] text-xs md:text-sm font-light text-wrap">
+                    Talha assisted me as a teaching assistant in one of my courses that I taught in Fall 2023 at HITEC University. He is a great person to work with. I wish him the best of luck for his future.
+                  </span>
+                  <span className="text-[#2c2ebf] text-3xl">″</span>
+                </p>
+                <p className="font-semibold py-3">Dr. Hasna Arshad</p>
+                <p className="text-xs font-light">Assistant Professor at HITEC</p>
+              </div>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <div className="border-x-[12px] border-[#0e0e1a] bg-[#060614] px-12 w-[100%] h-[70%] flex flex-col lg:flex-row items-center justify-evenly">
+              <div className="flex items-center justify-center w-[100%] lg:w-[20%]">
+                <img className="h-[120px] w-[120px]" src={UforiaLogo} alt="Uforia Logo" />
+              </div>
+              <div className="flex flex-col w-[80%] text-[#ffffff]">
+                <p>
+                  <span className="text-[#2c2ebf] text-3xl">″</span>
+                  <span className="text-[#ffffff] text-xs md:text-sm font-light text-wrap">
+                    Talha assisted me as a teaching assistant in one of my courses that I taught in Fall 2023 at HITEC University. He is a great person to work with. I wish him the best of luck for his future.
+                  </span>
+                  <span className="text-[#2c2ebf] text-3xl">″</span>
+                </p>
+                <p className="font-semibold py-3">Razia Hasan</p>
+                <p className="text-xs font-light">Chairperson of Help Humanity UK</p>
               </div>
             </div>
           </SwiperSlide>
