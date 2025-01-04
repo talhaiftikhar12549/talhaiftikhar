9c73ca9649fdf89908a7a71cd67df5d237280ebe
Talha <talhaiftikhar12549@gmail.com>
2025-01-04 20:29:33 +0500

    Added Raiza Tetamonial

--- src/components/GallerySection.jsx
+++ src/components/GallerySection.jsx
@@ -9,102 +9,128 @@ import "swiper/css/navigation";
 import "swiper/css/autoplay";
 
 const GallerySection = () => {
-  return (
-    <>
-      <div className="h-[100vh] sm:h-[80vh] lg:h-[60vh]  w-full bg-[#0e0e1a] flex items-center justify-center">
-        <Swiper
-          modules={[Pagination, Autoplay]}
-          autoplay={{ delay: 3000 }}
-          loop={true}
-          slidesPerView={1}
-          className="lg:w-[85%] xl:w-[90%] 2xl:w-[50%] h-[80%]"
-          pagination={{ clickable: true }}
-          style={{
-            '--swiper-pagination-color': '#2c2ebf',
-            '--swiper-pagination-bullet-inactive-color': '#cccccc',
-          }}
-        >
-          <SwiperSlide>
-            <div className="border-x-[12px] border-[#0e0e1a] bg-[#060614] px-4 w-[100%] h-[70%]  flex flex-col lg:flex-row items-center justify-evenly">
-              <div className="flex items-center justify-center w-[100%] lg:w-[20%]">
-                <img className="h-[120px] w-[120px]" src={usmanPic} alt="usmanPic" />
-              </div>
-              <div className="flex flex-col w-[80%] text-[#ffffff]">
-                <p>
-                  <span className="text-[#2c2ebf] text-3xl">″</span>
-                  <span className="text-[#ffffff] text-xs md:text-sm font-light text-wrap">
-                  I have worked with this buddy for a long time and have witnessed his growth in his career. We have collaborated on different projects, and he helped me rank a website on the first page.
-                  </span>
-                  <span className="text-[#2c2ebf] text-3xl">″</span>
-                </p>
-                <p className="font-semibold py-3">Usman Tariq</p>
-                <p className="text-xs font-light">UI/UX designer at Veroke</p>
-              </div>
-            </div>
-          </SwiperSlide>
+    return (
+        <>
 
-          <SwiperSlide>
-            <div className="border-x-[12px] border-[#0e0e1a] bg-[#060614] px-4 w-[100%] h-[70%]  flex flex-col lg:flex-row items-center justify-evenly">
-              <div className="flex items-center justify-center w-[100%] lg:w-[20%]">
-                <img className="h-[120px] w-[120px]" src={UforiaLogo} alt="Uforia Logo" />
-              </div>
-              <div className="flex flex-col w-[80%] text-[#ffffff]">
-                <p>
-                  <span className="text-[#2c2ebf] text-3xl">″</span>
-                  <span className="text-[#ffffff] text-xs md:text-sm font-light text-wrap">
-                    Using the Scrum Board has completely transformed the way our team collaborates.
-                    The intuitive design and seamless functionality make tracking tasks effortless.
-                  </span>
-                  <span className="text-[#2c2ebf] text-3xl">″</span>
-                </p>
-                <p className="font-semibold py-3">Usama Malik</p>
-                <p className="text-xs font-light">Software Engineer at Uforia</p>
-              </div>
-            </div>
-          </SwiperSlide>
+            <div id="projectsSection" className="h-[100%] bg-[#0e0e1a] text-[#d9d7d7] flex flex-col items-center py-10">
+                {/* {modalOpener && <ProjectModal  onClose={()=>setModalOpener(false)} />} */}
 
-          <SwiperSlide>
-            <div className="border-x-[12px] border-[#0e0e1a] bg-[#060614] px-4 w-[100%] h-[70%] flex flex-col lg:flex-row items-center justify-evenly">
-              <div className="flex items-center justify-center w-[100%] lg:w-[20%]">
-                <img className="h-[120px] w-[120px]" src={UforiaLogo} alt="Uforia Logo" />
-              </div>
-              <div className="flex flex-col w-[80%] text-[#ffffff]">
-                <p>
-                  <span className="text-[#2c2ebf] text-3xl">″</span>
-                  <span className="text-[#ffffff] text-xs md:text-sm font-light text-wrap">
-                    Talha assisted me as a teaching assistant in one of my courses that I taught in Fall 2023 at HITEC University. He is a great person to work with. I wish him the best of luck for his future.
-                  </span>
-                  <span className="text-[#2c2ebf] text-3xl">″</span>
-                </p>
-                <p className="font-semibold py-3">Dr. Hasna Arshad</p>
-                <p className="text-xs font-light">Assistant Professor at HITEC</p>
-              </div>
-            </div>
-          </SwiperSlide>
+                <div className="w-[90%] xl:w-[85%] 2xl:w-[70%] ">
+                    <div className="py-10 ">
+                        <p className=" text-3xl py-2 [#2c2ebf] relative">GALLERY. <span className='absolute bg-[#2c2ebf] bottom-0 left-0 h-[2px] w-[100px]'></span></p>
+                    </div>
+                    <div className=" py-8">
 
-          <SwiperSlide>
-            <div className="border-x-[12px] border-[#0e0e1a] bg-[#060614] px-12 w-[100%] h-[70%] flex flex-col lg:flex-row items-center justify-evenly">
-              <div className="flex items-center justify-center w-[100%] lg:w-[20%]">
-                <img className="h-[120px] w-[120px]" src={UforiaLogo} alt="Uforia Logo" />
-              </div>
-              <div className="flex flex-col w-[80%] text-[#ffffff]">
-                <p>
-                  <span className="text-[#2c2ebf] text-3xl">″</span>
-                  <span className="text-[#ffffff] text-xs md:text-sm font-light text-wrap">
-                    Talha assisted me as a teaching assistant in one of my courses that I taught in Fall 2023 at HITEC University. He is a great person to work with. I wish him the best of luck for his future.
-                  </span>
-                  <span className="text-[#2c2ebf] text-3xl">″</span>
-                </p>
-                <p className="font-semibold py-3">Razia Hasan</p>
-                <p className="text-xs font-light">Chairperson of Help Humanity UK</p>
-              </div>
+
+                        <div className="flex flex-col gap-12 py-6 w-[100%]">
+
+
+                            <Swiper
+                                modules={[Pagination, Autoplay]}
+                                autoplay={{ delay: 3000 }}
+                                loop={true}
+                                slidesPerView={1}
+                                className="lg:w-[85%] xl:w-[90%] 2xl:w-[50%] h-[80%]"
+                                pagination={{ clickable: true }}
+                                style={{
+                                    '--swiper-pagination-color': '#2c2ebf',
+                                    '--swiper-pagination-bullet-inactive-color': '#cccccc',
+                                }}
+                            >
+                                <SwiperSlide>
+                                    <div className="border-x-[12px] border-[#0e0e1a] bg-[#060614] px-4 w-[100%] h-[70%]  flex flex-col lg:flex-row items-center justify-evenly">
+                                        <div className="flex items-center justify-center w-[100%] lg:w-[20%]">
+                                            <img className="h-[120px] w-[120px]" src={usmanPic} alt="usmanPic" />
+                                        </div>
+                                        <div className="flex flex-col w-[80%] text-[#ffffff]">
+                                            <p>
+                                                <span className="text-[#2c2ebf] text-3xl">″</span>
+                                                <span className="text-[#ffffff] text-xs md:text-sm font-light text-wrap">
+                                                    I have worked with this buddy for a long time and have witnessed his growth in his career. We have collaborated on different projects, and he helped me rank a website on the first page.
+                                                </span>
+                                                <span className="text-[#2c2ebf] text-3xl">″</span>
+                                            </p>
+                                            <p className="font-semibold py-3">Usman Tariq</p>
+                                            <p className="text-xs font-light">UI/UX designer at Veroke</p>
+                                        </div>
+                                    </div>
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+                                    <div className="border-x-[12px] border-[#0e0e1a] bg-[#060614] px-4 w-[100%] h-[70%]  flex flex-col lg:flex-row items-center justify-evenly">
+                                        <div className="flex items-center justify-center w-[100%] lg:w-[20%]">
+                                            <img className="h-[120px] w-[120px]" src={usmanPic} alt="usmanPic" />
+                                        </div>
+                                        <div className="flex flex-col w-[80%] text-[#ffffff]">
+                                            <p>
+                                                <span className="text-[#2c2ebf] text-3xl">″</span>
+                                                <span className="text-[#ffffff] text-xs md:text-sm font-light text-wrap">
+                                                    I have worked with this buddy for a long time and have witnessed his growth in his career. We have collaborated on different projects, and he helped me rank a website on the first page.
+                                                </span>
+                                                <span className="text-[#2c2ebf] text-3xl">″</span>
+                                            </p>
+                                            <p className="font-semibold py-3">Usman Tariq</p>
+                                            <p className="text-xs font-light">UI/UX designer at Veroke</p>
+                                        </div>
+                                    </div>
+                                </SwiperSlide>
+                            </Swiper>
+
+                        </div>
+
+
+                    </div>
+                </div>
             </div>
-          </SwiperSlide>
-        </Swiper>
-      </div>
-      <div className="bg-[#2a2a33] h-[5px]"></div>
-    </>
-  );
+
+            {/* <div className="h-[100vh] sm:h-[80vh] lg:h-[60vh]  w-full bg-[#0e0e1a] flex flex-col items-center justify-center"> */}
+            {/* <div className="py-10 ">
+                <p className=" text-3xl py-2 [#2c2ebf] relative">CONTACT ME. <span className='absolute bg-[#2c2ebf] bottom-0 left-0 h-[2px] w-[100px]'></span></p>
+            </div> */}
+            {/* <Swiper
+                    modules={[Pagination, Autoplay]}
+                    autoplay={{ delay: 3000 }}
+                    loop={true}
+                    slidesPerView={1}
+                    className="lg:w-[85%] xl:w-[90%] 2xl:w-[50%] h-[80%]"
+                    pagination={{ clickable: true }}
+                    style={{
+                        '--swiper-pagination-color': '#2c2ebf',
+                        '--swiper-pagination-bullet-inactive-color': '#cccccc',
+                    }}
+                >
+                    <SwiperSlide>
+                        <div className="border-x-[12px] border-[#0e0e1a] bg-[#060614] px-4 w-[100%] h-[70%]  flex flex-col lg:flex-row items-center justify-evenly">
+                            <div className="flex items-center justify-center w-[100%] lg:w-[20%]">
+                                <img className="h-[120px] w-[120px]" src={usmanPic} alt="usmanPic" />
+                            </div>
+                            <div className="flex flex-col w-[80%] text-[#ffffff]">
+                                <p>
+                                    <span className="text-[#2c2ebf] text-3xl">″</span>
+                                    <span className="text-[#ffffff] text-xs md:text-sm font-light text-wrap">
+                                        I have worked with this buddy for a long time and have witnessed his growth in his career. We have collaborated on different projects, and he helped me rank a website on the first page.
+                                    </span>
+                                    <span className="text-[#2c2ebf] text-3xl">″</span>
+                                </p>
+                                <p className="font-semibold py-3">Usman Tariq</p>
+                                <p className="text-xs font-light">UI/UX designer at Veroke</p>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+
+                  
+                </Swiper> */}
+            {/* </div> */}
+
+
+
+
+
+
+
+            <div className="bg-[#2a2a33] h-[5px]"></div>
+        </>
+    );
 };
 
 export default GallerySection;
--- src/components/TestamonialSection.jsx
+++ src/components/TestamonialSection.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import UforiaLogo from "../assets/uforiainfotech_logo-removebg-preview.png";
 import usmanPic from "../assets/usmanPic.jpg";
+import raziaPic from "../assets/Capture2.png"
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import { Pagination, Autoplay } from "swiper/modules";
@@ -85,13 +86,13 @@ const Carousel = () => {
           <SwiperSlide>
             <div className="border-x-[12px] border-[#0e0e1a] bg-[#060614] px-12 w-[100%] h-[70%] flex flex-col lg:flex-row items-center justify-evenly">
               <div className="flex items-center justify-center w-[100%] lg:w-[20%]">
-                <img className="h-[120px] w-[120px]" src={UforiaLogo} alt="Uforia Logo" />
+                <img className="h-[120px] w-[120px]" src={raziaPic} alt="Uforia Logo" />
               </div>
               <div className="flex flex-col w-[80%] text-[#ffffff]">
                 <p>
                   <span className="text-[#2c2ebf] text-3xl">″</span>
                   <span className="text-[#ffffff] text-xs md:text-sm font-light text-wrap">
-                    Talha assisted me as a teaching assistant in one of my courses that I taught in Fall 2023 at HITEC University. He is a great person to work with. I wish him the best of luck for his future.
+                  For the past three years, he has handled all the tech work in this charity school. He is completely confident and delivers all the work on time. Currently, he is developing and maintaining the school's website.
                   </span>
                   <span className="text-[#2c2ebf] text-3xl">″</span>
                 </p>
