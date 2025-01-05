9dbb60ad7ddc000c4f22a1ab9ffb101c47c8d53b
talhaiftikhar12549 <talhaiftikhar12549@gmail.com>
2025-01-05 11:33:52 +0500

    Added responsiveness to Gallrey crousal

--- src/components/GallerySection.jsx
+++ src/components/GallerySection.jsx
@@ -13,50 +13,66 @@ const GallerySection = () => {
     return (
         <>
 
-            <div id="projectsSection" className="h-[100vh] bg-[#0e0e1a] text-[#d9d7d7] flex flex-col items-center py-10">
+            <div className="h-[100%] bg-[#0e0e1a] text-[#d9d7d7] flex flex-col items-center py-10">
 
 
-                <div className="w-[90%] xl:w-[85%] 2xl:w-[70%] h-[50vh] ">
+                <div className="w-[90%] xl:w-[85%] 2xl:w-[70%] h-[100%] ">
 
 
                     <div className="py-10 ">
-                        <p className=" text-3xl py-2 [#2c2ebf] relative">GALLERY. <span className='absolute bg-[#2c2ebf] bottom-0 left-0 h-[2px] w-[100px]'></span></p>
+                        <p className=" text-3xl py-2 relative">GALLERY. <span className='absolute bg-[#2c2ebf] bottom-0 left-0 h-[2px] w-[100px]'></span></p>
                     </div>
 
 
-                    <div className=" py-8 w-[100%] h-[60vh] flex items-center justify-center">
+                    <div className="w-[100%] h-[100%] flex items-center justify-center">
 
-                        <div className="flex flex-col gap-12 py-6 w-[80%] justify-center items-center">
-
+                        <div className="flex flex-col gap-12 py-8 h-[100%] w-[100%] justify-center items-center">
                             <Swiper
                                 modules={[Pagination, Autoplay, Navigation]}
                                 autoplay={{ delay: 3000, disableOnInteraction: false }}
                                 loop={true}
-                                slidesPerView={2}
-                                className="h-[80vh] w-full"
+                                spaceBetween={30}
+                                className="h-[80%] w-full"
+                                breakpoints={{
+                                    768: {
+                                      slidesPerView: 2, 
+                                    },
+                                    0: {
+                                      slidesPerView: 1, 
+                                    },
+                                  }}
                                 pagination={{ clickable: true }}
+                                navigation={true}
                                 style={{
                                     '--swiper-pagination-color': '#2c2ebf',
                                     '--swiper-pagination-bullet-inactive-color': '#cccccc',
+                                    '--swiper-navigation-color': '#2c2ebf',
+
                                 }}
                             >
                                 <SwiperSlide>
-                                    <div className=" border-[#0e0e1a] w-[100%] h-[100%]">
-                                        <img src={deskSetup2023} alt="Desk setup 2023" />
+                                    <div className="  ">
+                                        <img className="w-[100%] h-[50%]" src={deskSetup2023} alt="Desk setup 2023" />
                                     </div>
                                 </SwiperSlide>
-                                <SwiperSlide>
-                                    <div className=" border-[#0e0e1a] w-[100%] h-[100%]">
-                                        <img src={povatUfoia} alt="Povat UFOIA" />
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide>
-                                    <div className=" border-[#0e0e1a] w-[100%] h-[100%]">
-                                        <img src={povatUfoia} alt="Povat UFOIA duplicate" />
-                                    </div>
-                                </SwiperSlide>
-                            </Swiper>
 
+
+
+
+                                <SwiperSlide>
+                                    <div className=" ">
+                                        <img className="w-[100%] " src={povatUfoia} alt="Povat UFOIA" />
+                                    </div>
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+                                    <div className="">
+                                        <img className="w-[100%] " src={povatUfoia} alt="Povat UFOIA duplicate" />
+                                    </div>
+                                </SwiperSlide>
+
+
+                            </Swiper>
                         </div>
 
                     </div>
