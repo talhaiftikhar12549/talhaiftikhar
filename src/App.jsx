8580ac0e17cae0dd5c7302e54abfb6666e8b0e4e
talhaiftikhar12549 <talhaiftikhar12549@gmail.com>
2026-01-29 15:23:57 +0500

    added react package

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,50 @@
-import './App.css'
-import ParticlesComponent from './components/Particle';
-import ProjectSection from './components/ProjectSection';
-import NavBar from './components/NavBar';
-import HeroSection from './components/HeroSection';
-import AboutUs from './components/AboutUs';
-import MyExperience from './components/MyExperience';
-import EducationSkills from './components/Education&Skills';
-import ContactUs from './components/ContactUs';
-import FooterBar from './components/FooterBar';
-import Carousel from './components/TestamonialSection';
-import GallerySection from './components/GallerySection';
-import { useEffect } from 'react';
+import "./App.css";
+import ParticlesComponent from "./components/Particle";
+import ProjectSection from "./components/ProjectSection";
+import NavBar from "./components/NavBar";
+import HeroSection from "./components/HeroSection";
+import AboutUs from "./components/AboutUs";
+import MyExperience from "./components/MyExperience";
+import EducationSkills from "./components/Education&Skills";
+import ContactUs from "./components/ContactUs";
+import FooterBar from "./components/FooterBar";
+import Carousel from "./components/TestamonialSection";
+import GallerySection from "./components/GallerySection";
+import { useEffect } from "react";
+import { Helmet } from "react-helmet";
 function App() {
-useEffect(() => {
+  useEffect(() => {
     // 1. Get the current hostname
     const currentHost = window.location.hostname;
 
     // 2. Check if the user is on the OLD Netlify domain
     // (This ensures we don't break localhost or the new site)
     if (currentHost.includes("netlify.app")) {
-      
       // 3. Force the redirect to the new domain
       window.location.replace("https://www.talhaiftikhar.com");
     }
   }, []);
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Talha Iftikhar</title>
+        <link rel="canonical" href="https://www.talhaiftikhar.com/" />
+      </Helmet>
       {/* First section */}
-      <section className='h-[100vh] w-100'>
-
-        <div id='Home' className='top-0 left-0 right-0 bottom-0 absolute max-w-100 max-h-[90vh] md:max-h-[85vh] '>
-
+      <section className="h-[100vh] w-100">
+        <div
+          id="Home"
+          className="top-0 left-0 right-0 bottom-0 absolute max-w-100 max-h-[90vh] md:max-h-[85vh] "
+        >
           <NavBar />
 
           <ParticlesComponent id="particles" />
-
         </div>
 
         <div>
-
           <HeroSection />
-
         </div>
-
       </section>
 
       {/* First section */}
@@ -51,7 +53,6 @@ useEffect(() => {
       <AboutUs />
 
       {/* About US section */}
-
 
       {/* Experience Section */}
       <MyExperience />
@@ -65,7 +66,6 @@ useEffect(() => {
       <ProjectSection />
       {/* Projects Section */}
 
-
       {/* Education & Skills */}
       <EducationSkills />
       {/* Education & Skills */}
@@ -78,13 +78,11 @@ useEffect(() => {
       <ContactUs />
       {/* Contact Us Section */}
 
-
       {/* Footer Bar  */}
       <FooterBar />
       {/* Footer Bar  */}
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
